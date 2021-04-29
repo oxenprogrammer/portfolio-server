339e4751e560d837a37f7092a8f4f04f8873d2f7
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-29 22:34:56 +0300

    Feat(auth): add validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { User } from 'src/user/models/user.entity';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
+import { RegisterDTO } from './model/register.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private userService: UserService) {}
 
   @Post('register')
-  async register(@Body() body): Promise<User> {
-    const { password } = body;
+  async register(@Body() body: RegisterDTO): Promise<User> {
+    const { password, password_confirmation } = body;
+
+    if (password != password_confirmation) {
+      throw new BadRequestException('Passwords do not much');
+    }
+
     const hashed_password = await bcrypt.hash(password, 10);
     return await this.userService.create({
       ...body,
