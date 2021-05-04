adc9d2233579573c00aed2cfbbf115028dd95f60
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-04 23:14:10 +0300

    Feat(permission): add entity

---
+++ src/permission/permission.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('permissions')
+export class Permission {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+}
