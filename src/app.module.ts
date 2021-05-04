eded19383dd56956aa3b5c88c585eab98669376a
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-04 23:09:37 +0300

    Feat(permission): add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { RoleModule } from './role/role.module';
+import { PermissionModule } from './permission/permission.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule, CommonModule, RoleModule],
+  imports: [UserModule, DatabaseModule, AuthModule, CommonModule, RoleModule, PermissionModule],
   controllers: [AppController],
   providers: [AppService],
 })
