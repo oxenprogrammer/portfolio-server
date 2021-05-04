7a75ad5f90ac00b15d16a247f226a13be3f6531a
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-04 18:06:37 +0300

    Feat(role): add entity

---
+++ src/role/role.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  name: string;
+}
