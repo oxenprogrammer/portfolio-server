69d341a13e25ab6a8e692f460e559d3e7e669350
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-04 23:15:35 +0300

    Feat(permission): update module

--- src/permission/permission.module.ts
+++ src/permission/permission.module.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from 'src/common/common.module';
 import { Module } from '@nestjs/common';
-import { PermissionService } from './permission.service';
+import { Permission } from './permission.entity';
 import { PermissionController } from './permission.controller';
+import { PermissionService } from './permission.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Permission]), CommonModule],
   providers: [PermissionService],
   controllers: [PermissionController]
 })
