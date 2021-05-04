60b09c65ab6897f18e2661e73d67040ff0b6aee0
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-04 18:03:13 +0300

    Feat(role): add role module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule, CommonModule],
+  imports: [UserModule, DatabaseModule, AuthModule, CommonModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
