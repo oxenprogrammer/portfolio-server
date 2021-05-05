987227255b14461a7586379669a2329f02224d6c
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-06 02:39:51 +0300

    Feat(permission): update service

--- src/permission/permission.service.ts
+++ src/permission/permission.service.ts
@@ -1,25 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AbstractService } from 'src/common/abstract.service';
 import { Repository } from 'typeorm';
 import { PermissionDTO } from './model/permission.dto';
 import { Permission } from './permission.entity';
 
 @Injectable()
-export class PermissionService {
+export class PermissionService extends AbstractService {
   constructor(
     @InjectRepository(Permission)
     private readonly permissionRepository: Repository<Permission>,
-  ) {}
-
-  async all(): Promise<Permission[]> {
-    return await this.permissionRepository.find();
-  }
-
-  async create(data: PermissionDTO): Promise<Permission> {
-    return await this.permissionRepository.save(data);
-  }
-
-  async delete(id: string): Promise<any> {
-    return await this.permissionRepository.delete(id);
+  ) {
+    super(permissionRepository);
   }
 }
