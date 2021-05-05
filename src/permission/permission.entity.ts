e7c83f657a298c2b1bcdb6147924886047bf2313
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-05 19:31:39 +0300

    Feat(permission): add create method

--- src/permission/permission.entity.ts
+++ src/permission/permission.entity.ts
@@ -1,10 +1,14 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+import { IsEnum } from 'class-validator';
+import { PermissionEnum } from './permission.enum';
+
 @Entity('permissions')
 export class Permission {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column({ type: 'varchar' })
-  name: string;
+  @IsEnum(PermissionEnum)
+  name: PermissionEnum;
 }
