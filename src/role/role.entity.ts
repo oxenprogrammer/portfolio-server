a2ebe93cfc7ca7efe533950c72e8e11472b1876f
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-05 19:53:17 +0300

    Feat(permission): add manay to many relationship

--- src/role/role.entity.ts
+++ src/role/role.entity.ts
@@ -1,4 +1,12 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { Permission } from 'src/permission/permission.entity';
 
 @Entity('roles')
 export class Role {
@@ -7,4 +15,12 @@ export class Role {
 
   @Column({ type: 'varchar' })
   name: string;
+
+  @ManyToMany(() => Permission, { cascade: true })
+  @JoinTable({
+    name: 'role_permission',
+    joinColumn: { name: 'role_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'permission_id', referencedColumnName: 'id' },
+  })
+  permissions: Permission[];
 }
