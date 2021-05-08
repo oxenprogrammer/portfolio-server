4dc81c8005f37f5d772b66b217eecbfa2b64e3c2
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-08 17:45:00 +0300

    Feat(order): add service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,15 @@
+import { AbstractService } from 'src/common/abstract.service';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from './models/order.entity';
 
 @Injectable()
-export class OrderService {}
+export class OrderService extends AbstractService {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {
+    super(orderRepository);
+  }
+}
