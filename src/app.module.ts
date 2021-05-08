c9ce16b8f2b2f3c412d101527ee2a439300be2fb
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-08 16:34:23 +0300

    Feat(order): add order module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { CommonModule } from './common/common.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule, CommonModule, RoleModule, PermissionModule, ProductModule],
+  imports: [UserModule, DatabaseModule, AuthModule, CommonModule, RoleModule, PermissionModule, ProductModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
