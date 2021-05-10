ed9f162755cae0ac292343b7ccb73c520a8963b3
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-10 15:40:56 +0300

    Feat(project): add service

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -1,4 +1,15 @@
+import { AbstractService } from 'src/common/abstract.service';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './models/project.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService extends AbstractService {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {
+    super(projectRepository);
+  }
+}
