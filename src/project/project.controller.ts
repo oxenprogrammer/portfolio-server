c48d9fcc063c49855a10c7dbe891088c146ba962
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-10 15:49:50 +0300

    Feat(project): add create

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadGatewayException,
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerOptions } from 'src/common/multer-options';
+import { ProjectCreateDTO } from './models/project-create.dto';
+import { Project } from './models/project.entity';
+import { ProjectService } from './project.service';
 
 @Controller('project')
-export class ProjectController {}
+export class ProjectController {
+  constructor(private projectService: ProjectService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image', multerOptions))
+  async create(
+    @UploadedFile() file,
+    @Body() body: ProjectCreateDTO,
+  ): Promise<Project> {
+    const { title, desc, tag } = body;
+    try {
+      return await this.projectService.create({
+        title,
+        desc,
+        tag,
+        image: `http://127.0.0.1:8000/api/${file.path}`,
+      });
+    } catch (error) {
+      throw new BadGatewayException(error);
+    }
+  }
+}
