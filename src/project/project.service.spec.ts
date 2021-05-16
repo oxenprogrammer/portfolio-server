cd2cfee5f6c75f4a3bd8d795725157f278535d7e
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-16 16:55:43 +0300

    Test(service): add create && update

---
+++ src/project/project.service.spec.ts
@@ -0,0 +1,53 @@
+import { Project } from './models/project.entity';
+import { ProjectService } from './project.service';
+import { ProjectUpdateDTO } from './models/project-update.dto';
+import { Repository } from 'typeorm';
+import { Test } from '@nestjs/testing/test';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+describe('AbstractService', () => {
+  let projectService: ProjectService;
+  let repository: Repository<Project>;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        ProjectService,
+        {
+          provide: getRepositoryToken(Project),
+          useValue: {
+            all: jest.fn(),
+            create: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn(),
+            paginate: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+    projectService = module.get<ProjectService>(ProjectService);
+    repository = module.get<Repository<Project>>(getRepositoryToken(Project));
+  });
+
+  describe('Perform CRUD', () => {
+    const result = {
+      id: '1',
+      title: 'first project',
+      desc: 'description',
+      image: 'my image',
+      languages: ['image1', 'image2'],
+    };
+    it('should create a project', async () => {
+      const reposSpy = jest.spyOn(repository, 'create');
+      expect(projectService.create(result)).toBeTruthy;
+      expect(reposSpy).toBeCalledTimes(0);
+    });
+
+    it('should update a project', async () => {
+      const reposSpy = jest.spyOn(repository, 'update');
+      expect(projectService.update('1', result)).toBeTruthy;
+      expect(reposSpy).toBeCalledTimes(1);
+    });
+  });
+});
