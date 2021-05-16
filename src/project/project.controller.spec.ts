a6b4ffb414421e6e6224882a122913a0a3a9b68e
oxenprogrammer <emmy1000okello@gmail.com>
2021-05-16 13:30:07 +0300

    Test(project): add get all

--- src/project/project.controller.spec.ts
+++ src/project/project.controller.spec.ts
@@ -1,18 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Project } from './models/project.entity';
 import { ProjectController } from './project.controller';
+import { ProjectService } from './project.service';
+import { Repository } from 'typeorm';
 
 describe('ProjectController', () => {
-  let controller: ProjectController;
+  let projectService: ProjectService;
+  let projectController: ProjectController;
+  let prpojectRepository: Repository<Project>;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProjectController],
-    }).compile();
-
-    controller = module.get<ProjectController>(ProjectController);
+  beforeEach(() => {
+    projectService = new ProjectService(prpojectRepository);
+    projectController = new ProjectController(projectService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return a collection of projects', async () => {
+    const result = {
+      data: [],
+      meta: {
+        total: 0,
+        page: 1,
+        last_page: 0,
+      },
+    };
+    projectService.paginate = jest.fn().mockImplementation((): any => result);
+    expect(await projectController.all(1)).toEqual(result);
   });
 });
